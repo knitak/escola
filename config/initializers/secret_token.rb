f61764910f26e137dbdb06c02cac86f9927f47c1
Ana Pedrosa <anacpedrosa@gmail.com>
2013-01-21 23:25:56 +0000

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escola::Application.config.secret_token = '8dd092188fefba09564d17b150b66dfe57fc436b7a70d2c4b026df5852d6b9d67ba6f38cdd8b1fa11430f6c5ea5a7c6daa36fc56f5de7aab708bfe75afa77b3a'
