d87c043a38d2a0d68a1c2b8c58bcba27fc1dd5f3
Ana Pedrosa <anacpedrosa@gmail.com>
2013-01-22 00:34:24 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Escola::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/contactos"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
